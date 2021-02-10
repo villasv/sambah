0f976d3e4151cd811fb008bc9b6d602df904ef50
Victor Villas <mail@victor.villas>
2021-02-10 17:24:10 -0300

    Adiciona baixa do projeto e inserção no perfil

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
-#!/bin/sh
+#!/bin/bash -e
 
-# This is the only file that should be in english. It is read by the system that
-# is not ready to comprehend portuguese yet.
+# This is the only file that should be in english. It has to be readable by a
+# system that is not ready to comprehend portuguese yet, but should be at the
+# end of this script.
 
 SAMBAH_DIR="$HOME/.sambá" # Urgh... SAMBÁ_DIR is not a valid identifier!
-git clone https://github.com/villasv/sambah.git "$SAMBAH_DIR"
+if [ ! -d "$SAMBAH_DIR" ]; then
+    git clone https://github.com/villasv/sambah.git "$SAMBAH_DIR"
+fi
+
+# Add `source ~/.sambá/profile_pt_BR` to ~/.profile
+PROFILE="$HOME/.profile"
+if ! grep -qF 'sambá' "$PROFILE"; then
+    echo 'SAMBAH_DIR="$HOME/.sambá"' >> "$PROFILE"
+    echo '[ -s "$SAMBAH_DIR/.profile_pt_BR" ] && . "$SAMBAH_DIR/.profile_pt_BR"' >> "$PROFILE"
+fi
